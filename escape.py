eb0e24b52fc9470501272ebb02725ff2a1f46c6e
LiamSharpe <liamsmail1234@gmail.com>
2014-12-13 09:48:53 +1300

    Made the launch file and main game file.

---
+++ escape.py
@@ -0,0 +1,25 @@
+import sys
+
+class Escape(object):
+
+    def __init__(self):
+
+        print(sys.version)
+        self.createRooms()
+
+    def createRooms(self):
+        self.room = {}
+
+    def start(self):
+        print("Welcome to ESCAPE!!!!")
+        print("  Enjoy your stay.")
+        print("")
+        
+    def end(self):
+        print("Thank you for playing!")
+        playAgain = input("Would you like to play again (y/n)?")
+
+        if playAgain,lower() is "y":
+            self.start()
+        else:
+            sys.exit()
---
+++ run.py
@@ -0,0 +1,12 @@
+from escape import Escape
+
+class game(object):
+
+    def run(self):
+        self.game = Escape()
+        self.game.start()
+
+## Starting point
+if __name__ == "__main__":
+    app - Game()
+    app.run()
